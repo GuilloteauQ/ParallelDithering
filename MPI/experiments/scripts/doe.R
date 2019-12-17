aa4fb5599e2972b53dff5ee29017ff84875ec380
Quentin Guilloteau <Quentin.Guilloteau@grenoble-inp.org>
2019-12-17 10:48:52 +0100

    constraints the values

--- MPI/experiments/scripts/doe.R
+++ MPI/experiments/scripts/doe.R
@@ -4,11 +4,11 @@ if (!require("DoE.wrapper")) install.packages("DoE.wrapper");
 
 library("DoE.wrapper")
 
-number_of_runs <- 200
-min_image_size <- 8
-max_image_size <- 15
-max_block_size <- 15
-max_processors <- 32
+number_of_runs <- 2000
+min_image_size <- 15
+max_image_size <- 20
+max_block_size <- 10
+max_processors <- 6
 
 data_frame <- lhs.design(
                          type = "maximin",
@@ -28,6 +28,7 @@ pow2 <- function(n) {
 data_frame$image_size <- pow2(data_frame$image_size)
 data_frame$col_block_size <- pow2(data_frame$col_block_size)
 data_frame$row_block_size <- pow2(data_frame$row_block_size)
+data_frame$processors <- pow2(data_frame$processors)
 
 head(data_frame)
 
--- MPI/experiments/scripts/run_experiments.R
+++ MPI/experiments/scripts/run_experiments.R
@@ -4,9 +4,7 @@
 setwd("../..") # Root of the project
 experiments <- read.csv(file = "experiments/data/design_experiment.csv")
 
-valid_experiments <- experiments[experiments$col_block_size <= experiments$image_size &&
-                                 experiments$row_block_size <= experiments$image_size &&
-                                 (experiments$image_size / experiments$row_block_size) %% experiments$processors == 0,]
+valid_experiments <- experiments[(experiments$image_size / experiments$row_block_size) %% experiments$processors == 0,]
 
 head(valid_experiments)
 
@@ -16,10 +14,10 @@ system("make")
 mandrill_size <- 512
 tmp_image <- "Images/tmp.pgm"
 
-duplicates_commands <- paste("./duplicate_image", "Images/mandrill.pgm", tmp_image, valid_experiments$image_size / mandrill_size, sep = " ")
+duplicates_commands <- paste("./duplicate_image", "Images/mandrill.pgm", tmp_image, (valid_experiments$image_size / mandrill_size), sep = " ")
 
 commands <- paste(duplicates_commands, ";", "mpirun -np", valid_experiments$processors, "./dithering_bw", tmp_image, valid_experiments$col_block_size, valid_experiments$row_block_size, sep = " ")
 
 
-commands
-system(commands[1])
+head(commands)
+# system(commands[1])
