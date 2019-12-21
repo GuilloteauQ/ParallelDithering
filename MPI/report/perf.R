d8bd6f78c9dbd9b241bd7039be04cf5b75dc503f
Quentin Guilloteau <Quentin.Guilloteau@grenoble-inp.org>
2019-12-21 15:17:34 +0100

    fix perf

--- MPI/report/perf.R
+++ MPI/report/perf.R
@@ -1,7 +1,6 @@
 #!/usr/bin/env Rscript
 
 library(ggplot2)
-library(tidyr)
 library(dplyr)
 
 data_file <- "../experiments/data/output_rpi.csv"
@@ -14,7 +13,7 @@ head(df)
 
 # We need to compute the 95% interval for each quadruple (n, p, k, r)
 
-df %>% group_by(c(w, p, k, r)) %>% summarise(mean = mean(speedup), sd sd(speedup), min = min(speedup), max = max(speedup))
+df %>% group_by(w, p, k, r) %>% summarise(mean = mean(speedup), sd sd(speedup), min = min(speedup), max = max(speedup))
 
 df$conf_inter_inf <- df$mean - 2 * df$sd
 df$conf_inter_sup <- df$mean + 2 * df$sd
