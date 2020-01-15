cec09ac80a47c3781a637491cfdcd1504a59cb38
Quentin Guilloteau <Quentin.Guilloteau@grenoble-inp.org>
2020-01-15 16:32:31 +0100

    script for docker

---
+++ MPI/run_for_docker.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+IMAGE=Images/mandrill.pgm
+
+while [[ $# -gt 0 ]]
+do
+        key="$1"
+
+        case $key in
+            -p)
+                PROCESSES=$2
+                shift
+                shift
+                ;;
+            -k)
+                K=$2
+                shift
+                shift
+                ;;
+            -r)
+                R=$2
+                shift
+                shift
+                ;;
+            -f)
+                FACTOR=$2
+                shift
+                shift
+                ;;
+            --image)
+                IMAGE=$2
+                shift
+                shift
+                ;;
+            *)
+                echo "error while parsing the arguments"
+                exit
+                ;;
+        esac
+    done
+
+mpirun --allow-run-as-root -np ${PROCESSES} ./dithering_bw ${IMAGE} ${K} ${R} ${FACTOR}
+
+
+
